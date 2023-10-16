ac93e9c3dec425707b0c0e58b0f3fb33512a21f0
Thomas Paul <thomaspaul987@gmail.com>
2023-10-16 10:11:09 +0200

    ADD geo data

---
+++ data_processing/geo_dataset.R
@@ -0,0 +1,17 @@
+library(rworldmap)
+library(rgeos)
+
+# get world map
+wmap <- getMap(resolution="high")
+
+# get centroids
+centroids <- gCentroid(wmap, byid=TRUE)
+
+# get a dataframe with centroids
+df <- as.data.frame(centroids)
+head(df)
+
+write.csv(df, "../src/geo_data.csv")
+
+# plot
+plot(centroids)
