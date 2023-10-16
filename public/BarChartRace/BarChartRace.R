49485b68a1cc6358a5debba8a4395da7b77814c1
robin-grgi <georgi.robin@gmail.com>
2023-10-16 19:36:24 +0200

    bar chart race example

---
+++ public/BarChartRace/BarChartRace.R
@@ -0,0 +1,3 @@
+# Idée Globale : classer les pays en fonction des points de rank de l'API wasabi pour les chansons qui ont été produites par un artiste originaire de ce pays.
+# Ces données doivent défiler au cours du temps pour effetuer un bar chart race. 
+
