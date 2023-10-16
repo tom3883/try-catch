ac6f16df1b419001c03cac9eefe63b2ac3a2320c
yana <yana.ragozina@etu.unice.fr>
2023-10-16 09:54:30 +0200

    data preprocessing resoluton

--- data_processing/data_preprocessing.R
+++ data_processing/data_preprocessing.R
@@ -2,34 +2,32 @@ library(dplyr)
 
 # uploading datasets
 
-## artists <- read.csv("src/filtered_artists.csv", quote = "")
-## 
-## View(artists)
-## 
-## #artists_columns <- artists %>% select(name, location.country)
-## 
-## songs <- read.csv("src/filtered_songs.csv")
-## 
-## View(songs)
-## 
-## joined_data <- songs  %>% inner_join(artists, by = "name")
-## 
-## write.csv(joined_data, "src/dataset.csv", row.names = FALSE)
+artists <- read.csv("../src/filtered_artists.csv", quote = "")
 
+View(artists)
 
+#artists_columns <- artists %>% select(name, location.country)
+
+songs <- read.csv("../src/filtered_songs.csv")
+
+View(songs)
+
+#joined_data <- songs  %>% inner_join(artists, by = "name")
+
+#write.csv(joined_data, "src/dataset.csv", row.names = FALSE
 
 # data preprocessing
-
 # remove incoherent country values
-## val <- "[Worldwide]"
-## dataset <- dataset[dataset$'location.country' != val, ]
-## 
-## dataset$'location.country'[dataset$'location.country'== "Costa Mesa" ] <- "United States"
-## dataset$'location.country'[dataset$'location.country'== "Quebec" ] <- "Canada"
-## dataset$'location.country'[dataset$'location.country'== "São Paulo" ] <- "Brazil"
+dataset <- read.csv("../src/dataset.csv")
+val <- "[Worldwide]"
+dataset <- dataset[dataset$'location.country' != val, ]
+
+dataset$'location.country'[dataset$'location.country'== "Costa Mesa" ] <- "United States"
+dataset$'location.country'[dataset$'location.country'== "Quebec" ] <- "Canada"
+dataset$'location.country'[dataset$'location.country'== "São Paulo" ] <- "Brazil"
 
 print("Countries : \n")
 unique_values <- unique(dataset[['location.country']])
 print(unique_values)
-      
-      
+
+#write.csv(dataset, "src/dataset.csv", row.names = FALSE)
